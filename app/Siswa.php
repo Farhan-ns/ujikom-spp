2901b9bf1ea62d56e1bf5ea71a2f079de4d7e64d
farhan-ns <farhanunnasih@gmail.com>
2020-04-19 18:33:36 +0700

    modified Siswa model extend Authenticable

--- app/Siswa.php
+++ app/Siswa.php
@@ -2,9 +2,9 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Siswa extends Model
+class Siswa extends Authenticatable
 {
     protected $primaryKey = 'nisn';
 
